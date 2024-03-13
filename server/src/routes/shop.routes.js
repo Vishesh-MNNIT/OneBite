452909d6a8f24d462e0649f9d33fa7eba63da9d8
vishesh <visheshagrawal705@gmail.com>
2024-03-13 16:25:26 +0530

    routes errors fixed

--- server/src/routes/shop.routes.js
+++ server/src/routes/shop.routes.js
@@ -2,6 +2,6 @@ import { displayData } from "../controllers/shop.controller.js";
 import { Router } from "express";
 const router = Router();
 
-router.route("/displayData").get(displayData);
+router.route("/displayData").get(displayData);// buyer
 
 export default router;
