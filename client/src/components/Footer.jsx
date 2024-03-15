7e669058387dc1b957a94652a03519433914f309
the-panshu <rajputdeepanshuxyz1@gmail.com>
2024-03-15 13:30:38 +0530

    footer added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="text-center">Copyright &copy; 2024 One Bite</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
